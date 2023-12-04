928d9fd2694674446b529da04dcfd6bcaab97b06
shayyo <shay.yosef@gmail.com>
2023-12-04 15:42:09 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "server_app" {
-  ami            =  ami-06dd92ecc74fdfb36
-  instance_type  =  t2.micro
+  ami            =  "ami-06dd92ecc74fdfb36"
+  instance_type  =  "t2.micro"
   tags = {
     Name = "My EC2 instance"
   }
